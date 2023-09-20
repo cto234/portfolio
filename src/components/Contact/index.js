be220b2b7e29f5133ee42d04162c1bce6e25f196
cto234 <cto234@nyu.edu>
2023-09-20 11:45:14 -0400

    Fixed map. Adding initial parts of projects page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,15 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState} from 'react'
+import { MapContainer, TileLayer} from 'react-leaflet'
+import { useEffect, useState, useRef} from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 
     const contactArray = 'Contact me'.split('')
 
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
         const timeoutId = setTimeout(() => {
@@ -17,6 +20,25 @@ const Contact = () => {
           clearTimeout(timeoutId); // Clear the timeout when the component is unmounted
         };
       }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'service_14uh3ln', 
+            'template_v19ihqm', 
+            refForm.current, 
+            'Cqmrav7RBrKho55mL'
+            ).then(
+                () => {
+                    alert('Message sent successfully.')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Message send failure. Please try again.')
+                }
+            )
+    }
 
     return (
         <>
@@ -33,7 +55,7 @@ const Contact = () => {
                         Please feel free to contact me with any questions or inquiries you may have.
                     </p>
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input type='text' name='name' placeholder='Name' required />
@@ -53,6 +75,21 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                    Connor O'Doherty
+                    <br />
+                    Brooklyn, New York
+                    <br />
+                    <span>connor.t.odoherty@gmail.com</span>
+                </div>
+                <div className='map-wrap'>
+                    <MapContainer center={[40.7080993,-73.953759]} zoom={13} className='map-container'>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        />
+                    </MapContainer>
                 </div>
             </div>
         </>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,39 @@
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState} from 'react'
 
-const Projects = () => 'Hi'
+
+const Projects = () => {
+    
+    const projectArray = 'Projects'.split('')
+
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 3000);
+      
+        return () => {
+          clearTimeout(timeoutId); // Clear the timeout when the component is unmounted
+        };
+      }, []);
+    
+    return (
+        <>
+            <div className='container projects-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={projectArray}
+                        idx={15}
+                        />
+                    </h1>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default Projects
