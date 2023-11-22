a5ce8e24f4bcd2aa40ee1a56aa3c72995c43a7a8
cto234 <cto234@nyu.edu>
2023-11-21 19:40:01 -0500

    Added Connor's Book Reviews to projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters'
 import photoFolder from '../../assets/images/photo-folder.png'
 import blackjack from '../../assets/images/blackjack.png'
 import travelTime from '../../assets/images/travel-time.png'
+import bookReviews from '../../assets/images/bookReviews.png'
 import { useEffect, useState} from 'react'
 
 
@@ -35,23 +36,30 @@ const Projects = () => {
                     </h1>
                     <ul className='project-list'>
                         <li>  
+                            <a href='https://github.com/cto234/book-reviews' className='link'>
+                                <h2>Connor's Book Reviews</h2>
+                                <img className='screenshot' src={bookReviews} alt='Book Reviews'/>
+                            </a>
+                            <p>A little app built using React to let me rate and write reviews for the books I read.</p>
+                        </li>
+                        <li>  
                             <a href='https://github.com/cto234/travel-time' className='link'>
                                 <h2>Travel Time</h2>
-                                <img className='travel-time-image' src={travelTime} alt='Travel Time'/>
+                                <img className='screenshot' src={travelTime} alt='Travel Time'/>
                             </a>
-                            <p>A React app that calculates the travel times of different travelers such as animals or vehicles between any two points on earth using the Google Maps API.</p>
+                            <p>A React app that calculates the travel times of different travelers such as animals or vehicles between any two points on earth using the Google Maps API and Chakra UI.</p>
                         </li>
                         <li>  
                             <a href='https://github.com/cto234/Photo-Folder' className='link'>
                                 <h2>Photo Folder</h2>
-                                <img className='photo-folder-image' src={photoFolder} alt='Project Homepage'/>
+                                <img className='screenshot' src={photoFolder} alt='Project Homepage'/>
                             </a>
                             <p>A web app I made using Node, Express, MongoDB, Sass and Handlebars for storing and categorizing images from the web. Simply paste the url to the image in one of your folders to save it. (No email required to register, just create a unique username and password)</p>
                         </li>
                         <li>
                             <a href='https://github.com/cto234/Blackjack' className='link'>
                                 <h2>Blackjack</h2>
-                                <img className='blackjack-image' src={blackjack} alt='Blackjack' />
+                                <img className='screenshot' src={blackjack} alt='Blackjack' />
                             </a>
                             <p>A simple game of blackjack I made using Node, Express and Javascript for the CPU logic.</p>
                         </li>
